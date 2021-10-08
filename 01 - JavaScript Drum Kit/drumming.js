2615b5ce44fb12107d936453b2ee9521d236bfd4
matthiasffm <matthias@schmidt-dot.net>
2021-10-08 16:51:25 +0200

    feat: finished lesson 01

---
+++ 01 - JavaScript Drum Kit/drumming.js
@@ -0,0 +1,23 @@
+window.addEventListener('keydown', (ev) => {
+    var keyPressed = ev.key.toUpperCase();
+    if (keyPressed >= 'A' && keyPressed <= 'Z') {
+        const audioElem = document.querySelector(`audio[data-key=${keyPressed}]`);
+        if (audioElem) {
+            const audioFile = audioElem.getAttribute('src');
+            console.log('now play audio for ' + audioFile);
+            audioElem.currentTime = 0; // zurückspulen für mehrfache schnelle Tastendrücke eines Sounds direkt hintereinander
+            audioElem.play();
+            const divElemForKey = document.querySelector(`.note-key[data-key=${keyPressed}]`);
+            divElemForKey.classList.add('note-key-playing');
+        }
+    }
+});
+// Events registrieren für Ende der CSS-Transitions
+const keyButtons = document.querySelectorAll('.note-key');
+for (const keyButton of keyButtons) {
+    keyButton.addEventListener('transitionend', (te) => {
+        if (te.propertyName === 'transform') {
+            keyButton.classList.remove('note-key-playing');
+        }
+    });
+}
